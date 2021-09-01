1276c7e216f19492dfc301a2f60c0cf53eeeabc0
brianpompey <brianthescholar@gmail.com>
2021-08-31 23:03:11 -0400

    creating alarm function

--- alarmclock.py
+++ alarmclock.py
@@ -4,3 +4,8 @@ import time
 import winsound
 
 
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
