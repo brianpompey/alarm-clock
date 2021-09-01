1e943e37a25b26dc09fbd6be7ebddd8154b3b59b
brianpompey <brianthescholar@gmail.com>
2021-08-31 23:27:19 -0400

    some light design changes

--- alarmclock.py
+++ alarmclock.py
@@ -23,12 +23,11 @@ def actual_time():
     alarm(set_alarm_timer)
 
 clock = Tk()
-clock.title("DataFlair Alarm Clock")
-clock.iconbitmap(r"dataflair-logo.ico")
+clock.title("Brian's Alarm Clock")
 clock.geometry("400x200")
 time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
 addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
-setYourAlarm = Label(clock,text = "When to wake you up",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
+setYourAlarm = Label(clock,text = "When to wake you up",fg="blue",relief = "solid",font=("Helevetica",10,"bold")).place(x=0, y=30)
 
 # The Variables we require to set the alarm(initialization):
 hour = StringVar()
@@ -36,9 +35,9 @@ min = StringVar()
 sec = StringVar()
 
 #Time required to set the alarm clock:
-hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=110,y=30)
-minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=150,y=30)
-secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=30)
+hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=120,y=30)
+minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=160,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=210,y=30)
 
 #To take the time input by user:
 submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
